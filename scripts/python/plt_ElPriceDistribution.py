a4fa4a69d3c71fbf6151d1bd50ae0cde668e885f
juanjerezm <72557361+juanjerezm@users.noreply.github.com>
2024-12-15 22:16:30 -0300

    Enhance electricity price distribution plotting with mean line and utilities integration

--- scripts/python/plt_ElPriceDistribution.py
+++ scripts/python/plt_ElPriceDistribution.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+import utilities_plotting as utils_plot
 
 # ----- Matplotlib settings -----
 plt.rcParams["font.family"] = "Times New Roman"
@@ -58,14 +61,19 @@ def plot_histograms(data, bin_size=10, min_price=0, max_price=500):
         
         # Bar plot with bin edges as ticks between bars
         ax.hist(
-            year_data, bins=bin_edges, color="blue", alpha=0.7, edgecolor="none", label=f"Year {year}", density=True,
+            year_data, bins=bin_edges, color="blue", alpha=0.7, edgecolor="none", density=True,
         )
 
-        ax.set_title(f"{year}", fontsize=8, fontweight="bold")
-        # ax.set_ylabel("Frequency", fontsize=9)
+        ax.set_ylabel(f"{year}", fontsize=8, fontweight="bold")
         ax.grid(axis="y", linestyle="--", alpha=0.4)
 
-    fig.supylabel("Frequency", fontsize=8, fontweight="bold")
+        # Calculate and mark the mean
+        mean_value = year_data.mean()
+        ax.axvline(mean_value, color="red", linestyle="--", linewidth=1, label=f"Mean: {mean_value:.0f}")
+
+        # Add legend
+        ax.legend(fontsize=7, loc="upper right", frameon=False)
+
 
     # Create labels for the specific ticks
     label_ticks = {0, 50, 100, 150, 200, 250, 300}
@@ -88,12 +96,32 @@ def plot_histograms(data, bin_size=10, min_price=0, max_price=500):
     axes[-1].set_yticks(yticks)
     axes[-1].set_yticklabels([f"{int(y*100)}%" for y in yticks])
 
+    (_, _, x_center), (y_down, _, y_center) = utils_plot.axes_coordinates(axes)
+
+    fig.supylabel("Frequency", fontsize=8, fontweight="bold", y=y_center)
+
     plt.tight_layout()
-    plt.show()
+
+    # output
+    utils_plot.render_plot(
+        show=SHOW, save=SAVE, outdir=OUTDIR, plotname=PLOTNAME, dpi=DPI
+    )
 
 
 # Main execution
 if __name__ == "__main__":
+    SHOW = False
+    SAVE = True
+    OUTDIR = (
+        Path.home()
+        / "OneDrive - Danmarks Tekniske Universitet/Papers/J4 - article"
+        / "diagrams"
+        / "plots"
+    )
+    PLOTNAME = "ElectricityPriceDistribution"
+    DPI = 900
+
+
     data = process_data(file_paths)
     summary_stats = compute_summary_statistics(data)
     print(summary_stats)
